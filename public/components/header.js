83e0b439ec5410700915e76c58e872bfca874737
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-09-03 07:30:13 +0100

    fix: modified files

--- public/components/header.js
+++ public/components/header.js
@@ -5,7 +5,7 @@ class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
          
-    <header>
+  <header>
       <a href="./index.html">Books Trading Club</a>
         <input type="checkbox" id="toggle">
         <label for="toggle">&#9776;</label>
@@ -15,7 +15,9 @@ class Header extends HTMLElement {
                 <li><a href="./request.html">Request</a></li>
                 <li><a href="./trades.html">Trades</a></li>
                 <li><a href="./users.html">Users</a></li>
-                <li><a href="./profile.html" id="username"></a></li>
+                <li><a href="./login.html" id="username"></a>
+                <ul id="dropDown" ></ul>
+                </li>
             </ul>
         </nav>
     </header>
@@ -36,7 +38,7 @@ class Header extends HTMLElement {
         // User is not authenticated, show login form or redirect to the login page.
         let username = "login"
         displayUsername(username);
-        showLoginForm();
+       
       }
     } catch (error) {
       console.log(error);
@@ -45,16 +47,27 @@ class Header extends HTMLElement {
   
   function displayUsername(username) {
     const usernameElement = document.getElementById('username');
+    const dropDownElement = document.getElementById('dropDown');
     if (usernameElement) {
       usernameElement.textContent = username;
+      dropDownElement.appendChild('li') = showDropDown;
     }else{
       usernameElement.textContent = "login"
     }
   }
   
-  function showLoginForm() {
+  function showDropDown() {
     // Implement your logic to display the login form or redirect to the login page.
     // This might involve manipulating the DOM or changing the current URL.
+   const innerHTML = `
+        <ul>
+            <li> <a href="#">Profile</a></li>
+            <li> <a href="#">Edit profile</a></li>
+            <li> <a href="#">My Books</a></li>
+            <li> <a href="#">Log out</a></li>
+        </ul>
+    `
+    return innerHTML;
   }
   
   // Call the function to check authentication when the page loads.
